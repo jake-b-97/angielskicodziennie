749f52cc61fcb7fb2fb8c31921c10700af3882dd
Jakub <jbardzinski@icloud.com>
2021-05-02 16:32:07 +0100

    Initial Commit

---
+++ AngielskiCodziennie/AngielskiCodziennieApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngielskiCodziennieApp.swift
+//  AngielskiCodziennie
+//
+//  Created by Jakub Bardziński on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AngielskiCodziennieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
